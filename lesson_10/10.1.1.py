e45b17428f728e363dcc543736c4ba123a557191
StanleyIG <oootehts@gmail.com>
2022-07-10 11:09:32 +0300

    исправил 10.1.1

--- lesson_10/10.1.1.py
+++ lesson_10/10.1.1.py
@@ -3,16 +3,18 @@ class Matrix:
         self.matrix = matrix
 
     def __str__(self):
-        return '\n'.join(str(f'|{x[0]}  {x[1]}|') for x in self.matrix)
+        return '\n'.join('|' + '  '.join([f'{i}' for i in x]) + '|' for x in self.matrix)
+        #return '\n'.join(f'{x[0]} {x[1]} {x[2]}'for x in self.matrix)
+
 
     def __add__(self, other):
-        result = [[self.matrix[i][j] + self.matrix[i][j] for j in range
+        result = [[self.matrix[i][j] + other.matrix[i][j] for j in range
         (len(self.matrix[0]))] for i in range(len(self.matrix))]
-        return '\n'.join(f'|{i[0]}  {i[1]}|' for i in result)
+        return Matrix(result)
 
 
-m = Matrix([[12, 32], [25, 52],[78,45]])
-m_1 = Matrix([[12, 32], [25, 52], [78, 47]])
+m = Matrix([[12, 32, 25], [25, 52, 30],[78,45, 25]])
+m_1 = Matrix([[15, 40, 10], [35, 65, 10], [78, 47, 10]])
 print(m)
 print()
 print(m_1)
